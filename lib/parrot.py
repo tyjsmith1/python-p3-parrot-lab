4310c51935b3b67f2abf31067850d806ff8f4abb
tylersmith <tyjsmith1@gmail.com>
2023-12-09 09:44:00 -0600

    done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,8 @@
-def parrot():
-    pass
+def parrot(command=None):
+    default = "Squawk!"
+    if command is None:
+        print(default)
+        return default
+    else:
+        print(command)
+        return command
